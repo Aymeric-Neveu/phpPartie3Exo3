1d1003c1d8f1121d34df7d5de094ef5b6a679e70
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-19 11:00:37 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int = 100;
+    $int1 = 80;
+    while ($int >=10) {
+      echo $int * $int1;
+      $int--;
+    }
+    ?>
+  </body>
